5ce6f9d8c02a42418d0dd99f3d719d9b755bf6b8
Horse1jj <foste1jj.seth@gmail.com>
2025-03-14 18:03:21 -0400

    H

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ intents.messages = True
 intents.guilds = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="config.PREFIX", intents=intents)
 
 @bot.event
 async def on_ready():

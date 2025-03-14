41c5ccad4cad6fa0ed83b89474c777b4096de9d6
Horse1jj <foste1jj.seth@gmail.com>
2025-03-14 17:48:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import discord
+from discord.ext import commands
+import config
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await bot.load_extension("cogs.bounty")
+    await bot.load_extension("cogs.review")
+
+bot.run(config.TOKEN)
 
